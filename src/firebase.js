05b25e469abcc79fe497a404025513d3966f4b77
JPotterf <Jacob.Potterf@gmail.com>
2020-11-02 13:56:34 +0200

    standard create react app settings

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCT0N3tpigmChfPJSj9FV0Xv8PdjC2ktCo",
+    authDomain: "ecommerce-38e27.firebaseapp.com",
+    databaseURL: "https://ecommerce-38e27.firebaseio.com",
+    projectId: "ecommerce-38e27",
+    storageBucket: "ecommerce-38e27.appspot.com",
+    messagingSenderId: "563758142956",
+    appId: "1:563758142956:web:a3568ba8a38bf6170e1cc9",
+    measurementId: "G-MYFX38W21V"
+  };
